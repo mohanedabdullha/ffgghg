eb76ffdce9d308b2326df41e6ff6da722e35c549
mohanedabdullha <43791989+mohanedabdullha@users.noreply.github.com>
2018-11-06 22:54:44 +0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,12 @@
+
+const Discord = require('discord.js'),
+    bot = new Discord.Client({sisableEveryone: true})
+    console.log("muuuuuuuuute,");
+bot.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("ايدي الشات");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`**We .**`), 4000)        
+}
+});
+bot.login(process.env.BOT_TOKEN);
